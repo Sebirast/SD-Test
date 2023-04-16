bb46601dae249df5ecc61032903b9a2d6dca8f17
Sebastian Rast <sebastian.rast@outlook.com>
2023-04-16 16:15:54 +0200

    added test programm

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,62 @@
-#include <Arduino.h>
-#include "SD.h"
+#include <SD.h>
+#include <SPI.h>
+
+File myFile;
+
+const int chipSelect = BUILTIN_SDCARD;
+
+void setup()
+{
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect.
+  }
 
 
-void setup() {
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  
+  // open the file. 
+  SD.remove("test.txt");
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  byte test[] = {1, 2, 3, 51};
+  
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    // myFile.println("testing 1, 2, 3.");
+    myFile.write(test, 4);
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+  
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+    
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+    	Serial.println(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+  	// if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
 
-void loop() {
+void loop()
+{
+	// nothing happens after setup
 }
